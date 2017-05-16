7bdddf6e79c1aeffe1e03e1baf073d4c525b52b9
Magnus Holm <mail@magnusholm.com>
2017-05-16 14:01:07 +0100

    Add test for backstage passes with sellIn less than 10 days

--- spec/GildedRoseSpec.js
+++ spec/GildedRoseSpec.js
@@ -6,6 +6,12 @@ describe("Gilded Rose", function() {
     quality: 30
   }
 
+  var testBackstagePassSellIn10 = {
+    name: 'Backstage passes to a TAFKAL80ETC concert',
+    sellIn: 9,
+    quality: 30
+  }
+
   var testBackstagePass = {
     name: 'Backstage passes to a TAFKAL80ETC concert',
     sellIn: 11,
@@ -27,9 +33,14 @@ describe("Gilded Rose", function() {
   });
 
   describe('Backstage Passes', function() {
-    it('quality should go up when updated', function() {
+    it('quality goes up by 1 when updated', function() {
       shop.updateQualityBackstagePass(testBackstagePass)
       expect(testBackstagePass.quality).toEqual(31);
+    });
+
+    it('quality goes up by 2 if there are 10 or less days to sell in', function() {
+      shop.updateQualityBackstagePass(testBackstagePassSellIn10)
+      expect(testBackstagePassSellIn10.quality).toEqual(32);
     });
   });
 
